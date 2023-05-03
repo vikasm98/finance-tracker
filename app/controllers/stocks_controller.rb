dc64b3d9a650b45e404b3bc8de854037daca95bb
Vikas <svm@agilis.no>
2023-05-03 18:09:24 +0530

    respond to invalid stock search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,12 +9,16 @@ class StocksController < ApplicationController
           format.js { render partial: 'users/result' }
         end
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        flash.now[:alert] = "Please enter a valid symbol to search"
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      flash.now[:alert] = "Please enter a symbol to search"
+      respond_to do |format|
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 
