3abcef5ed22ecc2018d5679c3c6cf988fc7f3ec8
Vikas <svm@agilis.no>
2023-05-03 16:32:05 +0530

    submit form as XHR

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,15 @@
 class Stock < ApplicationRecord
 
-  def self.look_up(ticker_symbol)
+  def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex[:publishable_token],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.quote(ticker_symbol).latest_price
+
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
   end
 end
